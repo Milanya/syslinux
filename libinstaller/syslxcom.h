a7c0af4eeb88b3a80122afd0edb8a7243087df01
Shao Miller <shao.miller@yrdsb.edu.on.ca>
2011-02-09 23:34:35 -0500

    Run Nindent on libinstaller/syslxcom.h

--- libinstaller/syslxcom.h
+++ libinstaller/syslxcom.h
@@ -17,6 +17,6 @@ ssize_t xpread(int fd, void *buf, size_t count, off_t offset);
 ssize_t xpwrite(int fd, const void *buf, size_t count, off_t offset);
 void clear_attributes(int fd);
 void set_attributes(int fd);
-int sectmap(int fd, sector_t *sectors, int nsectors);
+int sectmap(int fd, sector_t * sectors, int nsectors);
 
 #endif
